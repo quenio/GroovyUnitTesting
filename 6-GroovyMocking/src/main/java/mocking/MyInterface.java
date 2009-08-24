ec8aa03f4698b4a2d671faa1a9c88634d66218e0
Quenio dos Santos <queniodossantos@gmail.com>
2009-08-23 23:20:53 -0500

    first commit

---
+++ 6-GroovyMocking/src/main/java/mocking/MyInterface.java
@@ -0,0 +1,10 @@
+package mocking;
+
+public interface MyInterface {
+
+	int one();
+	int one(int arg1);
+	int one(int arg1, int arg2);
+	int two();
+
+}
---
+++ 6-GroovyMocking/src/test/groovy/mocking/MyCallerTests.groovy
@@ -0,0 +1,30 @@
+package mocking;
+
+import groovy.mock.interceptor.MockFor
+
+public class MyCallerTests extends GroovyTestCase {
+	
+	public void testMyCaller() {
+		
+		def mock = new MockFor(MyInterface)   
+		
+		mock.demand.one(2..2) { 1 }                 
+		mock.demand.one() { arg -> arg + 1 }                 
+		mock.demand.one() { arg1, arg2 -> arg1 + arg2 }                 
+		mock.demand.two(0..2) { 2 }                 
+
+		def proxy = mock.proxyDelegateInstance()
+		def caller = new MyCaller(proxy) 
+		
+	    assert caller.callOne() == 1   
+	    assert caller.callOne() == 1   
+	    assert caller.callOne(10) == 11   
+	    assert caller.callOne(100,10) == 110   
+	    //assert caller.callTwo() == 2
+	    //assert caller.callTwo() == 2
+	    //assert caller.callTwo() == 2
+	    
+	    mock.verify(proxy)
+	}
+	
+}
